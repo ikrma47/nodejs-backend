77f6df5936a102c739f4b21ea664818c72b5b74d
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-10 23:20:48 +0500

    updated the dashboard api for required data

--- services/admin/dashboard/dashboard.js
+++ services/admin/dashboard/dashboard.js
@@ -1,12 +1,21 @@
-var { Departments } = require('../../../models/models');
+var { ApplicationStatus, Details } = require('../../../models/models');
 
 module.exports = async (req, res) => {
   try {
-    const departments = await Departments.findAll();
+    const submittedApplications = await ApplicationStatus.findAll({
+      where: { isSubmitted: true },
+      attributes: ['appId'],
+    });
+    const appIds = submittedApplications.map(({ appId }) => Number(appId));
+
+    const applicantsDetails = await Details.findAll({
+      where: { appId: appIds.length > 0 ? appIds : [0] },
+      attributes: ['appId', 'image', 'courseCategory', 'name'],
+    });
     res.json({
       success: true,
-      message: '',
-      data: [departments],
+      message: 'fetched successfully!',
+      data: [...applicantsDetails],
     });
   } catch (err) {
     console.log(err);
