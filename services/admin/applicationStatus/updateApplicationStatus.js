3e697e6a49ce40d78487ab1961a5c89ad86e512c
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-14 17:11:00 +0500

    added admin application status service

---
+++ services/admin/applicationStatus/updateApplicationStatus.js
@@ -0,0 +1,28 @@
+var { ApplicationStatus } = require('../../../models/models');
+var getApplicationStatus = require('../../common/applicationStatus/getApplicationStatus');
+
+module.exports = async (req, res) => {
+  const { appId } = req.params;
+  try {
+    const applicationStatus = await ApplicationStatus.update(
+      { ...req.body },
+      { where: { appId } },
+    );
+    if (applicationStatus) {
+      getApplicationStatus(req, res);
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'something gone wrong try again!',
+        data: [],
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: 'server error',
+      data: [],
+    });
+  }
+};
