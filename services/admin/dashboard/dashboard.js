65dcb00b00aaf2d76897e92d2eb7c75655bcf48d
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-14 17:13:24 +0500

    modified admin dashboard to get accepted and submmitted applicants

--- services/admin/dashboard/dashboard.js
+++ services/admin/dashboard/dashboard.js
@@ -3,19 +3,28 @@ var { ApplicationStatus, Details } = require('../../../models/models');
 module.exports = async (req, res) => {
   try {
     const submittedApplications = await ApplicationStatus.findAll({
-      where: { isSubmitted: true },
+      where: { isSubmitted: true, isAccepted: false },
       attributes: ['appId'],
     });
-    const appIds = submittedApplications.map(({ appId }) => Number(appId));
+    const acceptedApplications = await ApplicationStatus.findAll({
+      where: { isAccepted: true },
+      attributes: ['appId'],
+    });
+    const submittedAppIds = submittedApplications.map(({ appId }) => Number(appId));
+    const acceptedAppIds = acceptedApplications.map(({ appId }) => Number(appId));
 
-    const applicantsDetails = await Details.findAll({
-      where: { appId: appIds.length > 0 ? appIds : [0] },
+    const submittedApplicantsDetails = await Details.findAll({
+      where: { appId: submittedAppIds.length > 0 ? submittedAppIds : [0] },
+      attributes: ['appId', 'image', 'courseCategory', 'name'],
+    });
+    const acceptedApplicantsDetails = await Details.findAll({
+      where: { appId: acceptedAppIds.length > 0 ? acceptedAppIds : [0] },
       attributes: ['appId', 'image', 'courseCategory', 'name'],
     });
     res.json({
       success: true,
       message: 'fetched successfully!',
-      data: [...applicantsDetails],
+      data: [{ submittedApplicantsDetails }, { acceptedApplicantsDetails }],
     });
   } catch (err) {
     console.log(err);
