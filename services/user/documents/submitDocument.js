044660b6b1f61223913fa42ae07859e528b8cd50
ikrma47 <ikrmaahmad47@gmail.com>
2021-03-15 20:47:05 +0500

    fixed a typo

--- services/user/documents/submitDocument.js
+++ services/user/documents/submitDocument.js
@@ -1,11 +1,11 @@
-var { UploadedDocuments } = require('../../../models/models');
+var { UploadedDocument } = require('../../../models/models');
 
 module.exports = async (req, res) => {
   const bodyKeys = Object.keys(req.body);
   const bodyValues = Object.values(req.body);
 
   try {
-    const documents = await UploadedDocuments.findOne({ where: { appId: req.user.appId } });
+    const documents = await UploadedDocument.findOne({ where: { appId: req.user.appId } });
 
     bodyKeys.forEach((key, idx) => { documents[key] = bodyValues[idx]; });
     await documents.save();
