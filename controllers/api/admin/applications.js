20e38e37b0da246173c6985f3e337f2e59da4fd3
ikrma47 <ikrmaahmad47@gmail.com>
2021-06-04 09:16:22 +0500

    updated to get all applications using query string

--- controllers/api/admin/applications.js
+++ controllers/api/admin/applications.js
@@ -1,9 +1,18 @@
 var router = require('express').Router();
 var passport = require('passport');
-var getApplicationsByDepartmentAndCourseCategory = require('../../../services/admin/applications/getApplicationsByDepartmentAndCourseCategory');
-var getApplicationsByDepartment = require('../../../services/admin/applications/getApplicationsByDepartment');
+var getApplications = require('../../../services/admin/applications/getApplications');
+// var getApplicationsByDepartmentAndCourseCategory
+// = require('../../../services/admin/applications/getApplicationsByDepartmentAndCourseCategory');
+// var getApplicationsByDepartment
+// = require('../../../services/admin/applications/getApplicationsByDepartment');
 
-router.get('/:departmentName', passport.authenticate('jwt', { session: false }), getApplicationsByDepartment);
-router.get('/:departmentName/:courseCategory', passport.authenticate('jwt', { session: false }), getApplicationsByDepartmentAndCourseCategory);
+// router.get(
+// '/:departmentName',
+// passport.authenticate('jwt', { session: false }),
+// getApplicationsByDepartment
+// );
+// router.get('/:departmentName/:courseCategory',
+// passport.authenticate('jwt', { session: false }), getApplicationsByDepartmentAndCourseCategory);
+router.get('/all', passport.authenticate('jwt', { session: false }), getApplications);
 
 module.exports = router;
