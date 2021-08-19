50a9a7090e43b5637b364fbcf8fec407226d8e7b
Ikrma Ahmad <ikrmaahmad47@gmail.com>
2021-08-19 21:35:09 +0500

    added primary key to the course preference modal

---
+++ migrations/20210819081213-addingPrimaryKeyToCoursePreferenceModel.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('coursePreference', 'id', {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: true,
+      autoIncrement:true,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
