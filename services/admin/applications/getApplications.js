e207cdee9c2d114058907374ea00ac41fec12d78
Ikrma Ahmad <ikrmaahmad47@gmail.com>
2021-08-19 21:39:05 +0500

    implement outer join in dashboard service

--- services/admin/applications/getApplications.js
+++ services/admin/applications/getApplications.js
@@ -52,6 +52,7 @@ function getApplications({
               ),
             ],
           },
+          required: false,
           include: [
             { model: Experience, attributes: [] },
             {
