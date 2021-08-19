bbf6307dfeb62efc40ebf706eec0fe0f76c304c5
Ikrma Ahmad <ikrmaahmad47@gmail.com>
2021-08-19 21:41:44 +0500

    get department by batch offering admission in respective department

--- services/common/department/getDepartmentByBatch.js
+++ services/common/department/getDepartmentByBatch.js
@@ -1,21 +1,30 @@
+var { Op } = require('sequelize');
 var {
-  Departments, Batch, OfferedProgram, DepartmentCourse,
+  Departments, OfferedProgram, DepartmentCourse, Details,
 } = require('../../../models/models');
 
 module.exports = async (req, res) => {
-  const { batchYear } = req.params;
+  const { batchId } = req.params;
   try {
-    const data = await Batch.findAll({
-      where: { year: batchYear },
+    const { courseCategory = '%' } = await Details.findOne({
+      attributes: ['courseCategory'],
+      where: { appId: req.user.appId },
+    });
+    const data = await OfferedProgram.findAll({
+      where: { batchId },
+      attributes: ['id'],
       include: [{
-        model: OfferedProgram,
-        include: [{
-          model: DepartmentCourse,
-          include: [Departments],
-        }],
+        model: DepartmentCourse,
+        attributes: ['id'],
+        where: { courseCategory: { [Op.like]: courseCategory } },
+        include: [{ model: Departments, attributes: ['id', 'departmentName'] }],
       }],
     });
-    console.log(data.toJson());
+    res.json({
+      success: true,
+      message: 'fetched',
+      data,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
