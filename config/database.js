84683522b7af0dcff441ed4022b6471c22b588bc
Ikrma Ahmad <ikrmaahmad47@gmail.com>
2021-08-20 14:15:08 +0500

    deleted hard coded connection

--- config/database.js
+++
@@ -1,7 +0,0 @@
-var { Sequelize } = require('sequelize');
-
-module.exports = new Sequelize('pgs_admission', 'node_user', '12345', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-});
